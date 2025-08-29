568741ad7c183c65e51d379afb6ec4742892011a
Andy61490963 <lol901111@yahoo.com.tw>
2025-08-29 16:34:42 +0800

    更名專案名稱為WeYuNotify

--- WeYuTelegramNotify/Interfaces/ITelegramNotifyService.cs
+++
@@ -1,9 +0,0 @@
-using WeYuTelegramNotify.Models;
-
-namespace WeYuTelegramNotify.interfaces;
-
-public interface ITelegramNotifyService
-{
-    Task<TelegramSendResult> SendAsync(TelegramNotifyRequest request, CancellationToken cancellationToken = default);
-}
-
--- WeYuTelegramNotify/Options/TelegramBotOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace WeYuTelegramNotify.Options;
-
-public class TelegramBotOptions
-{
-    public string BaseUrl { get; set; } = string.Empty;
-    public string BotToken { get; set; } = string.Empty;
-}
-
